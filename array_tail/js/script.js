c9ad2a25c7ad469ebc8b5e64896e96bdead961aa
Aligern <sasso.luca93@gmail.com>
2024-03-12 16:54:45 +0100

    2nd snack done

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,13 +1,28 @@
 // we need to create an array and to ask the user how many items he wish to put inside it:
 
-// we start by creating the empty list
-let emptyList = [
+// we ask the user how many items he wish to put inside with the prompt:
+let userNumber = parseInt(prompt("Quanti elementi vuoi inserire nella lista?"));
+
+// we continue by creating the empty list
+let userList = [
 ];
 
-// we ask the user how many items he wish to put inside with the prompt:
-emptyList = parseInt(prompt("Quanti elementi vuoi inserire nella lista?"));
+// we do a for loop defined by the user prompt
+for (let i = 0; i <= userNumber; i++) {
+    console.log(i,'index');
+    let random =  getRndInteger(1,100);
+    console.log(random,'random');
+    userList.push(random);
+};
 
-for ( let i = 0; i )
+let userEndList = parseInt(prompt("quanti elementi vuoi selezionare dalla fine?"));
+let newArray = userList.slice(-userEndList);
+
+if (isNaN(userEndList)) {
+    console.log(userEndList);
+}
+
+console.log(newArray);
 
 
 // randomizer function:
