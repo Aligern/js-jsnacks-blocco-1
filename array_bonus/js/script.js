c0a3bd3f28407bc67540a7ead8f104cde3a1a479
Aligern <sasso.luca93@gmail.com>
2024-03-12 18:07:27 +0100

    snack 3 done

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,6 +1,4 @@
-// we need to create 2 array with differents lenghts
-
-
+// we need to create 2 array with differents lenghts:
 let array1 = [
     1,
     2,
@@ -8,21 +6,21 @@ let array1 = [
     4,
     5,
 ];
-
 let array2 = [
     1,
     2,
     3,
 ];
 
+// we define the difference between the two arrays we have via their lenght:
 const difference = array1.length - array2.length;
 console.log(difference);
 
-
 // now we must add to the shortest array as much element as needed to reach the longest one:
-for (let i = 0; i <= difference ; i++) {
-    if(array2 < array1) {
-        let randomPush = getRndInteger(4,10);
+for (let i = 0; i <= difference; i++) {
+    //we make the push untill array2 reaches the same lenght of array1 with a condition
+    if(array2.length < array1.length) {
+        let randomPush = getRndInteger(1,100);
         //console.log(randomPush);
         array2.push(randomPush);
     };
@@ -30,6 +28,8 @@ for (let i = 0; i <= difference ; i++) {
 console.log(array1,'array1');
 console.log(array2,'array2');
 
+
+// this is the randomizer function
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
