f29f1e109d9087cb9d8978d2905db60706035264
Aligern <sasso.luca93@gmail.com>
2024-03-12 15:25:06 +0100

    another commit

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,6 +1,16 @@
 // we need to create an array and to ask the user how many items he wish to put inside it:
 
+// we start by creating the empty list
 let emptyList = [
-
 ];
 
+// we ask the user how many items he wish to put inside with the prompt:
+emptyList = parseInt(prompt("Quanti elementi vuoi inserire nella lista?"));
+
+for ( let i = 0; i )
+
+
+// randomizer function:
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
